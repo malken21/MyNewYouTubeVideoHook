76bfb5990e1827ed3de78774bde2ddfd0595bd19
malken21 <77374813+malken21@users.noreply.github.com>
2024-08-16 19:23:47 +0900

    Create util.js

---
+++ util.js
@@ -0,0 +1,42 @@
+function getYouTubeService() {
+    return OAuth2.createService('YouTube')
+        .setAuthorizationBaseUrl('https://accounts.google.com/o/oauth2/auth')
+        .setTokenUrl('https://accounts.google.com/o/oauth2/token')
+        .setClientId(CLIENT_ID)
+        .setClientSecret(CLIENT_SECRET)
+        .setCallbackFunction('authCallback')
+        .setPropertyStore(PropertiesService.getScriptProperties())
+        .setScope('https://www.googleapis.com/auth/youtube.readonly');
+}
+
+function authCallback(request) {
+    const service = getYouTubeService();
+    const isAuthorized = service.handleCallback(request);
+    if (isAuthorized)
+        return HtmlService.createHtmlOutput('認証に成功しました。');
+    else
+        return HtmlService.createHtmlOutput('認証に失敗しました。');
+}
+
+function getDate() {
+    const time = PropertiesService.getScriptProperties().getProperty('lastCheckDate')
+    if (!time) {
+        const nowTime = new Date();
+        PropertiesService.getScriptProperties().setProperty('lastCheckDate', nowTime.toISOString());
+        return nowTime;
+    }
+    return new Date(time);
+}
+function setDate(time) {
+    PropertiesService.getScriptProperties().setProperty('lastCheckDate', time);
+}
+
+function sendDiscordNotification(video) {
+    const options = {
+        method: 'post',
+        contentType: 'application/json',
+        payload: JSON.stringify(payload(video))
+    };
+    UrlFetchApp.fetch(DISCORD_WEBHOOK_URL, options);
+    console.log(`${video.snippet.publishedAt}: https://youtu.be/${video.id.videoId}`);
+}
