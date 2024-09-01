a11adfb2d1605d5535ea76ab461b4f2a98ad2fa8
malken21 <77374813+malken21@users.noreply.github.com>
2024-09-01 14:49:54 +0900

    検索結果の動画の数を50に設定

--- code.js
+++ code.js
@@ -6,7 +6,7 @@ function checkNewVideos() {
 
     if (service.hasAccess()) {
         // 自分のチャンネルの動画をアップロードされた時間が新しい順で取得する URL
-        const url = `https://www.googleapis.com/youtube/v3/search?forMine=true&part=snippet&order=date&type=video`;
+        const url = `https://www.googleapis.com/youtube/v3/search?forMine=true&part=snippet&order=date&type=video&maxResults=50`;
         const response = UrlFetchApp.fetch(url, { headers: { Authorization: `Bearer ${service.getAccessToken()}` } });
         const data = JSON.parse(response.getContentText());
 
